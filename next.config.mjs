af1e2b9fe8e665a6ca7ed1c1f7eb9dd41bc55d6f
harsh-letsai-tech <harsh.dhariwal@letsai.tech>
2025-01-16 21:07:05 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  distDir: 'build',
-  output: 'standalone',
+    output: 'standalone',
+    experimental: {
+        outputFileTracingRoot: undefined,
+    }
 };
 
 export default nextConfig;
